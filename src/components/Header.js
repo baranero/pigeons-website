6f90e6ccebe332c7d961373b9f7f0652f4da6427
baranero <jakub.baran2@gmail.com>
2023-06-15 11:20:35 +0200

    gradient bg and hidden header while scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,43 @@
-import classes from './Header.module.css'
+import React, { useState, useEffect } from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
-    return (
-        <section className={classes.header}>
-            <div className={classes['header-logo']}>
-                <img className={classes.image} src="./images/logo.png" alt="Logo"/>
-                <h1>Krzysztof Baran</h1>
-            </div>
-            <nav>
-                <ul className={classes.navbar}>
-                    <li className={classes['navbar-item']}><a href="#about">O mnie</a></li>
-                    <li className={classes['navbar-item']}><a href="#achievements">Osiągniecia</a></li>
-                    <li className={classes['navbar-item']}><a href="#pigeons">Gołębie</a></li>
-                    <li className={classes['navbar-item']}><a href="#contact">Kontakt</a></li>
-                </ul>
-            </nav>
-        </section>
-    )
-}
+  const [hidden, setHidden] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
 
-export default Header
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        setHidden(false);
+      } else {
+        setHidden(true);
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
+  return (
+    <section className={`${classes.header} ${hidden ? classes.hidden : ''}`}>
+      <div className={classes['header-logo']}>
+        <img className={classes.image} src="./images/logo.png" alt="Logo" />
+        <h1>Krzysztof Baran</h1>
+      </div>
+      <nav>
+        <ul className={classes.navbar}>
+          <li className={classes['navbar-item']}><a href="#about">O mnie</a></li>
+          <li className={classes['navbar-item']}><a href="#achievements">Osiągniecia</a></li>
+          <li className={classes['navbar-item']}><a href="#pigeons">Gołębie</a></li>
+          <li className={classes['navbar-item']}><a href="#contact">Kontakt</a></li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default Header;
--- src/components/Pigeons.js
+++ src/components/Pigeons.js
@@ -8,7 +8,10 @@ const Pigeons = () => {
         <section id='pigeons' className={classes.pigeons}>
             <div className={classes.images}>
                 <ImageSlider slides={SliderData}/>
-                <p className={classes['images-description']}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <div className={classes['images-container']}>
+                <h2 className={classes.title}>Starannie wyselekcjowane linie gołębi</h2>
+                <p className={classes.description}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
             </div>
         </section>
     )
